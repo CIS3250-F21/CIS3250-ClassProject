584006514b9ab18343201c0342827a0cf1a4fa43
Eric Morse <ericm7597@gmail.com>
2021-11-02 20:19:51 -0400

    Create include files to reduce clutter

---
+++ groupIncludes.h
@@ -0,0 +1,22 @@
+#ifndef GROUP_INCLUDES_H
+#define GROUP_INCLUDES_H
+
+#include "Group1/Group1.h"
+#include "Group10/Group10.h"
+#include "Group11/Group11.h"
+#include "Group12/Group12.h"
+#include "Group13/Group13.h"
+#include "Group14/Group14.h"
+#include "Group15/Group15.h"
+#include "Group16/Group16.h"
+#include "Group17/Group17.h"
+#include "Group2/Group2.h"
+#include "Group3/Group3.h"
+#include "Group4/Group4.h"
+#include "Group5/Group5.h"
+#include "Group6/Group6.h"
+#include "Group7/Group7.h"
+#include "Group8/Group8.h"
+#include "Group9/Group9.h"
+
+#endif
---
+++ groupTestIncludes.h
@@ -0,0 +1,6 @@
+#ifndef GROUP_TEST_INCLUDES_H
+#define GROUP_TEST_INCLUDES_H
+
+// #include "Group7/Group7Tests.h"
+
+#endif

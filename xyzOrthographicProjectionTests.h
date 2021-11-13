f603a254dfad21380341eaa33d9896f704666be7
KyrenePineda <54953452+KyrenePineda@users.noreply.github.com>
2021-11-12 21:07:47 -0500

    Update and rename Group16/Group16Tests.h to xyzOrthographicProjectionTests.h

---
+++ xyzOrthographicProjectionTests.h
@@ -0,0 +1,11 @@
+#ifndef XYZ_ORTHOGRAPHIC_PROJECTION_TESTS_H
+#define XYZ_ORTHOGRAPHIC_PROJECTION_TESTS_H
+
+#include "../main.h"
+#include "xyzOrthographicProjection.h"
+
+void xyzOrthographicProjectionTest1.c();
+void xyzOrthographicProjectionTest2.c();
+void xyzOrthographicProjectionTest3.c();
+
+#endif

53a15e6aedefdf54529d446e2ea77a3b1cefdb06
Adhyayan Bhandari <bhana6491@gmail.com>
2021-11-13 15:39:11 -0500

    zScaling files

--- Group5/Group5.c
+++ Group5/Group5.c
@@ -1,4 +1,25 @@
 #include "Group5.h"
 
-void zScaling() {
+void zScaling(){
+
+  float vectorScalingValue = getZScale();
+  
+  for(int x = 0; x < inputShape->numOfVectors; x++){
+    float zValue;
+
+    struct point *newVector = getVector(x);
+
+    if (!newVector){ //checks that there is a valid vector at the index location
+        fprintf(stderr, "zScaling(): ERROR: No vector at given location\n");
+        return;
+    }
+    else{
+        zValue = newVector->element[2];
+        zValue *= vectorScalingValue;
+        newVector->element[2] = zValue;
+        setVector(x, newVector);
+        
+    }
+  }
+  return;
 }
---
+++ Group5/Group5Tests.h
@@ -0,0 +1,6 @@
+void runGroup5Tests();      //test function
+void correctScale();        //clean
+void zeroMulti();           //dirty 1
+void negativeShapeScale();  //dirty 2
+void overflow();            //dirty 3
+void incrementVectors();    //dirty 5

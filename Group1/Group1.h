112e9131b1982c6e55f3ca004d4b2bf0c54f9331
vdixon531 <72442684+vdixon531@users.noreply.github.com>
2021-11-12 19:40:35 -0500

    Update Group1.h
    Updated Group1.h

--- Group1/Group1.h
+++ Group1/Group1.h
@@ -2,7 +2,30 @@
 #define GROUP_1_H
 
 #include "../main.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/stat.h>
+
+struct points{
+	float vector[4];
+};
+
+struct shape{ // Declaration of shape structure.
+	struct points* vectors;
+	int numOfVectors;
+	float scaling[4];
+	float rotation[3];
+	float translation[3];
+	float shearing[3];
+};
 
 struct shape *readInput(char* fileName);
+char test1(char [], char []);
+int test2(char *, char *);
+int test3(char *, char *);
+int test4(char *, char *);
+int test5(char []);
+int test6(char [], char []);
 
 #endif

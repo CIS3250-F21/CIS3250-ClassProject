593c8364cf6d4a06aa33c09c8211cf6fdb3562d6
hhxmna <hamna2285@gmail.com>
2021-11-14 14:23:35 -0500

    YPlaneReflection.c

--- Group11/Group11.c
+++ Group11/Group11.c
@@ -1,4 +1,36 @@
-#include "Group11.h"
+#include "YPlaneReflection.h"
+#include "../main.h"
 
 void yPlaneReflection() {
+  
+  // edit global float matrix 
+	int i, j;
+
+	struct point localVector;
+	struct point* vect;
+
+	j = 1, i = 0;
+	// get vector
+	while (j) {
+		vect = getVector(i);
+		// if getVector returns Null break the loop 
+		if (vect == NULL) {
+			j = 0;
+		}
+		// otherwise transform vector
+		else {
+			// multiply vector with transformation matrix 
+			multiplyMatrix(vect);
+
+			localVector.element[0] = vect->element[0];
+			localVector.element[1] = vect->element[1];
+			localVector.element[2] = vect->element[2];
+			localVector.element[3] = vect->element[3];
+			// set updated local vector back using set 
+			setVector(i, localVector);
+			// increment index 
+			i++;
+		}
+	}
+
 }

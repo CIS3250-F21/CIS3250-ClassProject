0c8fa89328d5c5c20e4fb5c9d3a49a691d616004
Ikeoluwa Agbaje <iagbaje@fred.socs.uoguelph.ca>
2021-11-14 20:28:29 -0500

    Updated Group8.c and Group8.h files

--- Group8/Group8.c
+++ Group8/Group8.c
@@ -1,7 +1,32 @@
 #include "Group8.h"
 
 void zRotation() {
+  
+    float theta;
+    theta = getZRotation ();
+    generateZRotationMatrix (theta);
+    struct point currentElement;
+
+    for (int i = 0 ; i < inputShape -> numOfPoints ; i++) {
+      
+        currentElement = *(getPoint (i));
+
+        multiplyMatrix (&currentElement, transformationMatrix);
+
+        setPoint (i, &currentElement);
+    
+    }
+
 }
 
 void generateZRotationMatrix(float theta) {
+
+    for (int i = 0 ; i < 4 ; i++)  {
+
+        for (int j = 0 ; j < 4; j++) {
+
+            transformationMatrix[i][j] = 0;
+
+        }
+    }
 }

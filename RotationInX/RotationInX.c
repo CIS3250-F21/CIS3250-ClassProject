bd86ff7733c9b2356f7fa90d95049fa6540dbec5
nnourr <nourqwerty123@gmail.com>
2021-11-17 00:14:07 -0500

    added testing and implementation code

--- RotationInX/RotationInX.c
+++ RotationInX/RotationInX.c
@@ -1,7 +1,44 @@
 #include "RotationInX.h"
+#include "../main.h"
 
 void xRotation() {
+
+    generateXRotationMatrix(getXRotation());
+    
+    struct point* currPoint;
+
+    for (int i = 0; i < inputShape -> numOfPoints; i++) {
+		
+        currPoint = getPoint(i);
+		
+        if (currPoint == NULL) {
+          return;
+        }
+
+        multiplyMatrix(currPoint, transformationMatrix);
+        setPoint(i, currPoint);
+  }
 }
 
 void generateXRotationMatrix(float theta) {
+
+  transformationMatrix[0][0] = 1;
+  transformationMatrix[0][1] = 0;
+  transformationMatrix[0][2] = 0;
+  transformationMatrix[0][3] = 0;
+
+  transformationMatrix[1][0] = 0;
+  transformationMatrix[1][1] = cos(theta);
+  transformationMatrix[1][2] = (- (sin(theta)));
+  transformationMatrix[1][3] = 0;
+
+  transformationMatrix[2][0] = sin(theta);
+  transformationMatrix[2][1] = 0;
+  transformationMatrix[2][2] = cos(theta);
+  transformationMatrix[2][3] = 0;
+
+  transformationMatrix[3][0] = 0;
+  transformationMatrix[3][1] = 0;
+  transformationMatrix[3][2] = 0;
+  transformationMatrix[3][3] = 1;
 }

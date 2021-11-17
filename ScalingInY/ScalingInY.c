0d43564b93596d82f72cd34d8972209ac7548738
Jennifer Lithgow <jlithgow@fred.socs.uoguelph.ca>
2021-11-17 12:19:00 -0500

    added logic comment for yScaling, hid any print statements as necessary, tests are uncommented

--- ScalingInY/ScalingInY.c
+++ ScalingInY/ScalingInY.c
@@ -9,7 +9,21 @@
  */
 
 /* Modifies the y value of each element of the point list by multiplying the
- * current y value by the y scale value*/
+ * current y value by the y scale value
+ * looking at transformation from https://www.tutorialspoint.com/computer_graphics/3d_transformation.htm:
+ * Vector P = [x y z 1]
+ * Scaling factor S = 
+ *  | Sx 0  0  0 |
+ *  | 0  Sy 0  0 |
+ *  | 0  0  Sz 0 |
+ *  | 0  0  0  1 |
+ * P' = P.S
+ * P' = [x.Sx  y.Sy  z.Sz  1]
+ * In case of yScaling, x and z are not transformed (meaning Sx = Sz = 1)
+ * P' = [x(1)  y.Sy  z(1)  1]
+ * Since only y is modified by a variable value, yScaling transforms the matrix by focusing on
+ * only the y value of the point
+ */
 int yScaling() {
     struct point *curPoint;      // Holds current point
     float yScale = getYScale();  // Y scale used to modify points

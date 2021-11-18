dddebfa27a393c14e3d3bdd10998f880107c4539
Christian Catalano <catalanc@uoguelph.ca>
2021-11-18 12:36:50 -0500

    Added check in X,Y,Z Reflection Functions
    - Wont run if X,Y,Z reflection float is 0 in inputShape

--- XPlaneReflection/XPlaneReflection.c
+++ XPlaneReflection/XPlaneReflection.c
@@ -2,17 +2,20 @@
 #include "../main.h"
 
 void xPlaneReflection() {
-    generateXPlaneReflectionMatrix();
-    struct point* p;
-    int i = 0;
+    // If X Reflection was selected in transformation file
+    if( getXReflection != 0 ) {
+        generateXPlaneReflectionMatrix();
+        struct point* p;
+        int i = 0;
 
-	// Loop through each point negating x-values
-    while ((p = getPoint(i)) != NULL)
-    {
-		multiplyMatrix( p, transformationMatrix );
-		setPoint( i, p );
-        i++;
-	}
+        // Loop through each point negating x-values
+        while ((p = getPoint(i)) != NULL)
+        {
+            multiplyMatrix( p, transformationMatrix );
+            setPoint( i, p );
+            i++;
+        }
+    }
 }
 
 // Generates identity matrix with -1 in [0][0] element

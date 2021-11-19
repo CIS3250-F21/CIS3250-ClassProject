6b5d2e4b79f9b3555195c9ba19cab0eefab0a478
Dawoud-Husain <dawoudahusain@gmail.com>
2021-11-18 21:43:53 -0500

    fixing valgrind errors

--- ShearInY/ShearInYTests.c
+++ ShearInY/ShearInYTests.c
@@ -3,19 +3,46 @@
 
 //The following are dirty test cases for group 14
 
-void emptyYShearValue () {
+void maximumFloatShearValueTest() {
+    float xShear = FLT_MAX;
+    float zShear = FLT_MAX;
 
     struct point *point = getPoint(0);
-    point = NULL;
+    point->element[0] = 3;
+    point->element[1] = 2;
+    point->element[2] = 4;
+    point->element[3] = 1;
+
+    struct point *expectedPoint = malloc(sizeof(struct point));
+    expectedPoint->element[0] = point->element[0] + (xShear * point->element[1]); 
+    expectedPoint->element[2] = point->element[2] + (xShear * point->element[2]); 
+    expectedPoint->element[1] = 6; 
+    expectedPoint->element[3] = 1; 
 
     setPoint(0, point);
-    setYShear(NULL);
+    setXShear(xShear);
+    setZShear(zShear);
+    yShear();
     
-    if(getPoint(1) == NULL) {
-        printf ("Error: The input value of the y shear is empty\n");
+    if(getPoint(0)->element[0] != expectedPoint->element[0] || getPoint(0)->element[2] != expectedPoint->element[2]) {
+        printf ("Error: The maximum float shear value is reached yeilding an unexpected result\n");
     }
+    free(expectedPoint);
 }
 
+
+// void emptyYShearValue () {
+
+//     struct point *point = getPoint(0);
+//     point = NULL;
+
+//     setPoint(0, point);
+//     setYShear();
+    
+//     if(getPoint(1) == NULL) {
+//         printf ("Error: The input value of the y shear is empty\n");
+//     }
+// }
 
 //Test Two: The input is out of the range of the matrix/vector
 void inputRange () { 
@@ -62,7 +89,7 @@ void uninitializedMatrixMultiplication(){
   //Test Five: The x or z shear values are empty
   void dependentGroupShearValues () {
    float xShear = 5;
-    float zShear = 4;
+   float zShear = 4;
 
     struct point *point = getPoint(0);
     point = NULL;
@@ -74,9 +101,7 @@ void uninitializedMatrixMultiplication(){
     
     if(getPoint(0) == NULL || getPoint(2) == NULL) {
         printf ("Error: The input value of the x or z shear is empty.\n");
-        //fprintf(stderr, "nullPointTest Failed: Expected point does not match test point\n");
     }
-
   }
 
   //The following is a clean test cases for group 14
@@ -126,7 +151,6 @@ void uninitializedMatrixMultiplication(){
     
   }
 }
-
 
 void runYShearTests () {
   correctTest();

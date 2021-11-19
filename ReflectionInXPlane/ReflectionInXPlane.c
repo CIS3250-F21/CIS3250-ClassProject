b2189eb1ed267bc30084b968168ff1de221027f5
Christian Catalano <catalanc@uoguelph.ca>
2021-11-19 13:55:33 -0500

    Added overflow check to functionality

--- ReflectionInXPlane/ReflectionInXPlane.c
+++ ReflectionInXPlane/ReflectionInXPlane.c
@@ -5,10 +5,24 @@ void ReflectionInXPlane() {
     generateXPlaneReflectionMatrix();
     struct point* p;
     int i = 0;
+    int overUnderFlow = 0;
 
     // Loop through each point negating x-values
     while ((p = getPoint(i)) != NULL)
     {
+        for (int o = 0; o < 4; o ++) {
+            if (p->element[o] < -FLT_MAX || p->element[o] > FLT_MAX) {
+                overUnderFlow = 1;
+                break;
+            } else if ((p->element[o] < FLT_MIN && p->element[o] > 0) || (p->element[o] > -FLT_MIN && p->element[o] < 0)) {
+                overUnderFlow = 1;
+                break;
+            }
+        }
+        if (overUnderFlow == 1) {
+          break;
+        }
+        
         multiplyMatrix( p, transformationMatrix );
         setPoint( i, p );
         i++;
--- ReflectionInXPlane/ReflectionInXPlane.h
+++ ReflectionInXPlane/ReflectionInXPlane.h
@@ -1,6 +1,8 @@
 #ifndef X_PLANE_REFLECTION_H
 #define X_PLANE_REFLECTION_H
 
+#include <float.h>
+
 void ReflectionInXPlane();
 void generateXPlaneReflectionMatrix();
 

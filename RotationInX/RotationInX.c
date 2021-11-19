a8fe573c68b7385b4e09e42be57cb66f408bfc55
andrewlinington <aliningt@uoguelph.ca>
2021-11-19 13:25:34 -0500

    adjust rotation to have reset matrix and changed readME

--- RotationInX/RotationInX.c
+++ RotationInX/RotationInX.c
@@ -18,23 +18,13 @@ void generateXRotationMatrix(float theta) {
 
   // apply the rotation matrix formula to the transformationMatrix 
   // with the given theta
-  transformationMatrix[0][0] = 1;
-  transformationMatrix[0][1] = 0;
-  transformationMatrix[0][2] = 0;
-  transformationMatrix[0][3] = 0;
+  
+  resetMatrix();
 
-  transformationMatrix[1][0] = 0;
   transformationMatrix[1][1] = cos(theta);
   transformationMatrix[1][2] = (- (sin(theta)));
-  transformationMatrix[1][3] = 0;
 
-  transformationMatrix[2][0] = 0;
   transformationMatrix[2][1] = sin(theta);
   transformationMatrix[2][2] = cos(theta);
-  transformationMatrix[2][3] = 0;
-
-  transformationMatrix[3][0] = 0;
-  transformationMatrix[3][1] = 0;
-  transformationMatrix[3][2] = 0;
-  transformationMatrix[3][3] = 1;
+  
 }

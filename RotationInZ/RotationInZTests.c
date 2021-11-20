e989699828fdc2360def2694dc20a5fb4d46cfd3
Ikeoluwa Agbaje <iagbaje@fred.socs.uoguelph.ca>
2021-11-19 21:15:56 -0500

    Updated test files

--- RotationInZ/RotationInZTests.c
+++ RotationInZ/RotationInZTests.c
@@ -1,153 +1,128 @@
 #include "RotationInZTests.h"
 
+//Runs tests
 void runRotationInZTests() {
 
-    /*
+    testAngleEqualtoZero ();
+    testAnglesWithDecimalPlaces ();
+    testAngleWithMaxFloatValue ();
+    testAngleWithMinFloatValue ();
+    testAngleWithSqrt ();
+    testValidVectors ();
 
-        Function calls to the testValidAngles function that test if the generateZRotationMatrix works as it should for valid input. 
-        The first two calls to this test function are the 'clean' tests because they are expected values 
-        well in the range of possible parameters.
-        The next three are the 'dirty' tests because they test more unlikely inputs such as angles given in scientific notation. These are all still valid inputs.
-        All of the tests should be successful because the generateZRotationMatrix should accept any real number as a valid angle.
-
-        float expected is a local variable that represents the expected matrix that the generateZRotationMatrix function should produce
-        It's values are hard coded since they are known before the tests are run 
-        (except sin and cos calculations which are done by calling the appropriate function from the math.h library)
-        All of the values of the expected transformation matrix should have a value of 0 besides:
-            index[0][0] = cos (angle)
-            index[0][1] = -sin (angle)
-            index[1][0] = sin (angle)
-            index[1][1] = cos (angle)
-            index[2][2] = 1
-            index[3][3] = 1
-
-    */
-    float expected[4][4];
-
-    for (int i = 0; i < 4; i++)
-    {
-        for (int j = 0; j < 4; j++)
-        {
-            expected [i][j] = 0;
-        }
-    }
-
-    expected[2][2] = 1;
-    expected[3][3] = 1;
-
-    // Initializing the values of the expected matrix and then testing the generateZRotationMatrix using an angle in radians of 0
-    expected[0][0] = cos (0);
-    expected[0][1] = -sin (0);
-    expected[1][0] = sin (0);
-    expected[1][1] = cos (0);
-    setZRotation (0);
-    testValidAngles (expected, getZRotation ());
-
-    // Initializing the values of the expected matrix and then testing the generateZRotationMatrix using an angle in radians of 790.28
-    expected[0][0] = cos (790.28);
-    expected[0][1] = -sin (790.28);
-    expected[1][0] = sin (790.28);
-    expected[1][1] = cos (790.28);
-    setZRotation (790.28);
-    testValidAngles (expected, getZRotation ());
-
-    // Initializing the values of the expected matrix and then testing the generateZRotationMatrix using an angle in radians of 1.17549E39
-    expected[0][0] = cos (1.17549E39);
-    expected[0][1] = -sin (1.17549E39);
-    expected[1][0] = sin (1.17549E39);
-    expected[1][1] = cos (1.17549E39);
-    setZRotation (1.17549E39);
-    testValidAngles (expected, getZRotation ());
-
-    // Initializing the values of the expected matrix and then testing the generateZRotationMatrix using an angle in radians of -1.17549E-39
-    expected[0][0] = cos (-1.17549E-39);
-    expected[0][1] = -sin (-1.17549E-39);
-    expected[1][0] = sin (-1.17549E-39);
-    expected[1][1] = cos (-1.17549E-39);
-    setZRotation (-1.17549E-39);
-    testValidAngles (expected, getZRotation ());
-
-    // Initializing the values of the expected matrix and then testing the generateZRotationMatrix using an angle in radians of sqrt(99)
-    expected[0][0] = cos (sqrt(99));
-    expected[0][1] = -sin (sqrt(99));
-    expected[1][0] = sin (sqrt(99));
-    expected[1][1] = cos (sqrt(99));
-    setZRotation (sqrt(99));
-    testValidAngles (expected, getZRotation ());
-
-    // Finished testing valid angles
-
-
-    /*
-
-        Function calls to the testValidVectors function that test if the zRotation works as it should for valid input. 
-        The call to this test function is a 'clean' tests because they are expected values 
-        well in the range of possible parameters.
-        
-    */
-
-
-    // The value of the global vector is initialized to match the vector that is being tested by the zRotation function
-    // If the function works properly, these values will change to match the expected vector
-    setZRotation(3.14/2);
-
-    // Initializing the values of the expected vector and then testing the zRotation on the vector [0, 1, 2, 1] and the angle 3.14/2
-    // The expected vector after rotation should have values of {0.9999, 0.0008, 2, 1}
-    float expectedVector[4] = {0.9999, 0.0008, 2, 1};
-    testValidVectors (expectedVector);
-
-    //Finished valid vectors testing
 }
 
+// Clean test
+// Test rotation matrix with an angel equavalent to zero
+void testAngleEqualtoZero () {
+  
+    float expectedMatrix[4][4] = {{cos(0), -sin(0), 0, 0}, {sin(0), cos(0), 0, 0}, {0, 0, 1, 0}, {0, 0, 0, 1}}; 
 
-
-
-/*
-
-                                                 TESTING VALID ANGLES FUNCTION
- This test function will test the generateZRotationMatrix function using valid (real number) angles. The resulting matrix will be compared  
- with another matirx (that has the expected matrix values). If both matrices match meaning the generateZRotationMatrix worked properly, 
- nothing happens. If the actual transformationMatrix is different from the expected matrix, an error message is printed.              
- This test function will be run by passing a wide range of floating point values (including the minimum and maximum range values of floats) 
-
-*/
-
-void testValidAngles (float expectedTransformationMatrix[4][4], float theta) {
-
-    int header = 0;
-
-    generateZRotationMatrix (theta);
+    generateZRotationMatrix (0);
 
     for (int i = 0 ; i < 4 ; i++) {
 
         for (int j = 0 ; j < 4 ; j++) {
 
-            if ((fabs (transformationMatrix[i][j] - expectedTransformationMatrix[i][j])) > 0.001) {
+            if ((fabs (transformationMatrix[i][j] - expectedMatrix[i][j])) > 0.001) {
 
-                if (header == 0) {
-
-                    printf ("\nFUNCTION generateZRotationMatrix: VALID ANGLES TEST FAILED - theta = %f", theta);
-                    header = 1;
-
-                }
-
-                printf ("\nExpected index[%d][%d] = %f\nActual index[%d][%d] = %f\n", i, j, expectedTransformationMatrix[i][j], i, j, transformationMatrix[i][j]);
+                printf ("Test rotation matrix with an angel equavalent to zero failed.\n");
             }
         }
-    }    
+    } 
 }
 
+// Clean Test
+// Test rotation matrix with a decimal value
+void testAnglesWithDecimalPlaces () {
+  
+    float expectedMatrix[4][4] = {{cos(790.28), -sin(790.28), 0, 0}, {sin(790.28), cos(790.28), 0, 0}, {0, 0, 1, 0}, {0, 0, 0, 1}}; 
 
-/*
+    generateZRotationMatrix (790.28);
 
-                                                 TESTING VALID VECTORS FUNCTION
- This test function will test the zRotation function using valid (real numbers) vectors. The resulting vectors will be compared with 
- the expected vectors values. If both vectors match meaning the zRotation worked properly, nothing happens. If the actual vector is 
- different from the expected vector, an error message is printed. 
+    for (int i = 0 ; i < 4 ; i++) {
 
-*/
+        for (int j = 0 ; j < 4 ; j++) {
 
-void testValidVectors (float expectedVector [4]) {
+            if ((fabs (transformationMatrix[i][j] - expectedMatrix[i][j])) > 0.001) {
+
+                printf ("Test rotation matrix with a decimal value failed.\n");
+
+            }
+        }
+    } 
+}
+
+// Dirty Test
+// Test rotation matrix with the maximum float value
+void testAngleWithMaxFloatValue () {
+  
+    float expectedMatrix[4][4] = {{cos(1.17549E39), -sin(1.17549E39), 0, 0}, {sin(1.17549E39), cos(1.17549E39), 0, 0}, {0, 0, 1, 0}, {0, 0, 0, 1}}; 
+    
+    generateZRotationMatrix (1.17549E39);
+
+    for (int i = 0 ; i < 4 ; i++) {
+
+        for (int j = 0 ; j < 4 ; j++) {
+
+            if ((fabs (transformationMatrix[i][j] - expectedMatrix[i][j])) > 0.001) {
+
+                printf ("Test rotation matrix with the maximum float value failed.\n");
+
+            }
+        }
+    } 
+}
+
+// Dirty Test
+// Test rotation matrix with the minimum float value
+void testAngleWithMinFloatValue () {
+  
+    float expectedMatrix[4][4] = {{cos(-1.17549E-39), -sin(-1.17549E-39), 0, 0}, {sin(-1.17549E-39), cos(-1.17549E-39), 0, 0}, {0, 0, 1, 0}, {0, 0, 0, 1}}; 
+
+    generateZRotationMatrix (-1.17549E-39);
+
+    for (int i = 0 ; i < 4 ; i++) {
+
+        for (int j = 0 ; j < 4 ; j++) {
+
+            if ((fabs (transformationMatrix[i][j] - expectedMatrix[i][j])) > 0.001) {
+
+                printf ("Test rotation matrix with the minimum float value failed.\n");
+
+            }
+        }
+    } 
+}
+
+// Dirty Test
+// Test rotation matrix with squarerooted angle
+void testAngleWithSqrt () {
+  
+    float expectedMatrix[4][4] = {{cos(sqrt(99)), -sin(sqrt(99)), 0, 0}, {sin(sqrt(99)), cos(sqrt(99)), 0, 0}, {0, 0, 1, 0}, {0, 0, 0, 1}}; 
+    
+    generateZRotationMatrix (sqrt(99));
+
+    for (int i = 0 ; i < 4 ; i++) {
+
+        for (int j = 0 ; j < 4 ; j++) {
+
+            if ((fabs (transformationMatrix[i][j] - expectedMatrix[i][j])) > 0.001) {
+
+                printf ("Test rotation matrix with squarerooted angle failed.\n");
+
+            }
+        }
+    } 
+}
+
+// Clean test
+// Test vector with valid values for rotation matrix angle
+void testValidVectors () {
+
+    float expectedVector[4] = {0.9999, 0.0008, 2, 1};
+
+    setZRotation(3.14/2);
 
     zRotation ();
 
@@ -155,10 +130,12 @@ void testValidVectors (float expectedVector [4]) {
 
         if ((fabs ((getPoint(0) -> element[i]) - expectedVector[i])) > 0.001) {
 
-            printf ("\nVALID VECTOR TEST FAILED:\nExpected index[%d] = %f\nActual index[%d] = %f\n", i, expectedVector[i], i, (getPoint(0)) -> element[i]);
+            printf ("Test vector with valid values for rotation matrix angle failed.\n");
+
+            return;
 
         }
-    }
 
+    }
 
 }

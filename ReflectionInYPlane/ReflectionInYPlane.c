03d77ff9548097ce79be7b86d56063d8c3c3fce0
Christian Catalano <catalanc@uoguelph.ca>
2021-11-19 23:19:05 -0500

    Added proper files after improper merge

--- ReflectionInYPlane/ReflectionInYPlane.c
+++ ReflectionInYPlane/ReflectionInYPlane.c
@@ -1,39 +1,3 @@
-<<<<<<< HEAD:YPlaneReflection/YPlaneReflection.c
-#include "YPlaneReflection.h"
-
-void yPlaneReflection() {
-	generateYPlaneReflectionMatrix();
-	struct point* vect;
-	int j = 1;
-
-	int index = 0;
-
-	while (j) {
-		// get the vector from shapes
-		vect = getPoint(index);
-
-		// if getVector returns NULL then break the loop
-		if (vect == NULL) {
-			j = 0;
-		}
-		// otherwise transform the vector
-		else {
-			// multiply vector with transform matrix
-			multiplyMatrix(vect, transformationMatrix);
-			// store result of multiplication in local vector structure
-			// set the updated local vector back to shape using setVector
-			setPoint(index, vect);
-			// increment index
-			index++;
-		}
-	}
-}
-
-void generateYPlaneReflectionMatrix() {
-    resetMatrix();
-        transformationMatrix[1][1] = -1;
-}
-=======
 #include "ReflectionInYPlane.h"
 
 void ReflectionInYPlane() {
@@ -68,4 +32,3 @@ void generateYPlaneReflectionMatrix() {
     resetMatrix();
         transformationMatrix[1][1] = -1;
 }
->>>>>>> e751d3211daa75cc0957f736a3fd064f754c7f07:ReflectionInYPlane/ReflectionInYPlane.c
--- ReflectionInYPlane/ReflectionInYPlane.h
+++ ReflectionInYPlane/ReflectionInYPlane.h
@@ -1,14 +1,3 @@
-<<<<<<< HEAD:YPlaneReflection/YPlaneReflection.h
-#ifndef Y_PLANE_REFLECTION_H
-#define Y_PLANE_REFLECTION_H
-
-#include "../main.h"
-
-void yPlaneReflection();
-void generateYPlaneReflectionMatrix();
-
-#endif
-=======
 #ifndef Y_PLANE_REFLECTION_H
 #define Y_PLANE_REFLECTION_H
 
@@ -18,4 +7,3 @@ void ReflectionInYPlane();
 void generateYPlaneReflectionMatrix();
 
 #endif
->>>>>>> e751d3211daa75cc0957f736a3fd064f754c7f07:ReflectionInYPlane/ReflectionInYPlane.h
